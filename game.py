65b918b2d3215e5c0a027578a20c3088cb37a557
ArthurAtfield <arthuratfieldbiggs@gmail.com>
2026-06-25 12:26:58 +0100

    test

--- game.py
+++ game.py
@@ -3,11 +3,13 @@ import pygame
 class Game:
     def __init__(self):
         self.width = 1000
+        self.backgroundImg = pygame.image.load("img/start screen")
 
     def run(self):
         pygame.init()
+    
 
-WIDTH, HEIGHT = 1024, 640
+WIDTH, HEIGHT = 1024, 768
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Pygame Window")
 
